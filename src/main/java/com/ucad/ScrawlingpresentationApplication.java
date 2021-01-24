83ee0b8d8777e25eb5038ba50888953fe43e1dfd
Mamadou_Ndiaye <ndiamamadou@gmail.com>
2021-01-24 02:37:35 +0100

    Connexion a ma base de donnees dans le cloud Mongodb cloud atlas

--- src/main/java/com/ucad/ScrawlingpresentationApplication.java
+++ src/main/java/com/ucad/ScrawlingpresentationApplication.java
@@ -7,18 +7,25 @@ import com.ucad.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
+
 public class ScrawlingpresentationApplication {
     String[] profile= new String[0];
 
     public static void main(String[] args) {
         SpringApplication.run(ScrawlingpresentationApplication.class, args);
     }
+
+
+
    @Autowired
    UtilisateurRepository utilisateurRepository;
 
